d97c22cb83c9e9e0078084828c280d46b1b45137
voron521 <voron521@mail.ru>
2024-06-11 20:18:53 +0300

    correct

--- src/components/loaderAllTickets/loaderAllTickets.js
+++ src/components/loaderAllTickets/loaderAllTickets.js
@@ -1,10 +1,6 @@
 import './loaderAllTickets.css';
 
-import { Alert, Flex, Spin } from 'antd';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { LoadingOutlined } from '@ant-design/icons';
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
+import { Flex, Spin } from 'antd';
 
 function LoaderAllTickets() {
   const contentStyle = {
@@ -17,22 +13,11 @@ function LoaderAllTickets() {
     <div className="loader_all_tickets">
       <Flex gap="small" vertical>
         <Flex gap="small">
-          {/* <Spin tip="Loading" size="small">
-          {content}
-        </Spin> */}
-          {/* <Spin tip="Loading">{content}</Spin> */}
           <Spin tip="Loading" size="large">
             {content}
             <span>Догружаем все билеты из базы</span>
           </Spin>
         </Flex>
-        {/* <Spin tip="Загружаем все билеты...">
-        <Alert
-          message="Alert message title"
-          description="Further details about the context of this alert."
-          type="info"
-        />
-      </Spin> */}
       </Flex>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+
 import Aviasales from './components/AviaSales';
 import store from './store';
+
 const root = createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
